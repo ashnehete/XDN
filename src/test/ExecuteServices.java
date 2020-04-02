c56befb6d8611998fb72d23e998cef2c7b8eae8e
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-04-02 05:09:29 -0400

    update xdn app

--- src/test/ExecuteServices.java
+++ src/test/ExecuteServices.java
@@ -31,7 +31,7 @@ public class ExecuteServices {
         int id = 0;
 
         System.out.println("Start testing... ");
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<1000; i++) {
             int sent = 0;
             HttpActiveReplicaRequest req = new HttpActiveReplicaRequest(HttpActiveReplicaPacketType.EXECUTE,
                     testServiceName,
@@ -51,7 +51,7 @@ public class ExecuteServices {
                         , new RequestCallback() {
                             @Override
                             public void handleResponse(Request response) {
-                                System.out.println("Response received:" + response);
+                                System.out.println(System.currentTimeMillis()-start);
                                 received = 1;
                             }
                         });
@@ -67,6 +67,6 @@ public class ExecuteServices {
             received = 0;
         }
 
-        // client.close();
+        client.close();
     }
 }
