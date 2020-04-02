f8d2c02811a602fc55d261062cafcfcf66088b5e
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-04-02 05:10:29 -0400

    update test

--- src/test/ExecuteServices.java
+++ src/test/ExecuteServices.java
@@ -32,7 +32,7 @@ public class ExecuteServices {
 
         System.out.println("Start testing... ");
         for (int i=0; i<1000; i++) {
-            int sent = 0;
+            int sent = 1;
             HttpActiveReplicaRequest req = new HttpActiveReplicaRequest(HttpActiveReplicaPacketType.EXECUTE,
                     testServiceName,
                     id++,
