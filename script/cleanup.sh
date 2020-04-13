f0ecdb38b9d877e0d97a293ddbc22a7f9ff57533
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-04-13 09:11:18 -0400

    update clear script

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo rm -r /tmp/gigapaxos gigapaxos.log* derby.log
+sudo rm -rf /tmp/gigapaxos gigapaxos.log* derby.log
 
 docker stop $(docker ps -a -q)
 docker rm -f $(docker ps -a -q)
 docker system prune -f -a
+docker volume rm $(docker volume volume ls -q)
