012405892a8b3949ea534ff95b2fd37137129a83
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-04-23 01:49:35 -0400

    update script

--- script/stopall.sh
+++ script/stopall.sh
@@ -7,10 +7,5 @@ while read p; do
 	ssh oversky@$p "rm -rf /tmp/gigapaxos/*" < /dev/null &
 	ssh oversky@$p "pkill -f ReconfigurableNode" < /dev/null &
 	
-	# clean up docker 
-	ssh oversky@$p "docker stop $(docker ps -a -q)" </dev/null 
-	ssh oversky@$p "docker rm -f $(docker ps -a -q)" < /dev/null &
-	# docker system prune -f -a
-	# docker volume rm $(docker volume ls -q)
 done <cl_ssh
 
