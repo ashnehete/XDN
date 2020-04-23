f838442293bc878320f04c7e43a4d46d111dffbd
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-04-23 02:38:32 -0400

    update script

--- script/stopall.sh
+++ script/stopall.sh
@@ -4,8 +4,8 @@ total=$1
 cnt=0
 while read p; do
 	echo $p
-	ssh oversky@$p "rm -rf /tmp/gigapaxos/*" < /dev/null &
-	ssh oversky@$p "pkill -f ReconfigurableNode" < /dev/null &
+	ssh oversky@$p "rm -rf /tmp/gigapaxos/*" < /dev/null 
+	ssh oversky@$p "pkill -f ReconfigurableNode" < /dev/null 
 	
 done <cl_ssh
 
