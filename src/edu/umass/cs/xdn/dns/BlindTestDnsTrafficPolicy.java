c531f365021683b51f7dded8be432a5f787b3326
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-05-10 17:33:46 -0400

    add a dns traffic policy for blind test

---
+++ src/edu/umass/cs/xdn/dns/BlindTestDnsTrafficPolicy.java
@@ -0,0 +1,39 @@
+package edu.umass.cs.xdn.dns;
+
+import edu.umass.cs.reconfiguration.dns.DnsTrafficPolicy;
+import edu.umass.cs.xdn.XDNConfig;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This policy is used for a blind test demo only
+ */
+public class BlindTestDnsTrafficPolicy implements DnsTrafficPolicy {
+
+    private static String sourceIP;
+
+    public BlindTestDnsTrafficPolicy(){
+        sourceIP = XDNConfig.prop.getProperty(XDNConfig.XC.EDGE_ADDR.toString());
+    }
+
+    @Override
+    public Set<InetAddress> getAddresses(Set<InetAddress> addresses, InetAddress source) {
+        Set<InetAddress> result = new HashSet<InetAddress>();
+        // convert the set to a list
+        List<InetAddress> targetList = new ArrayList<>(addresses);
+
+        if ( source.toString().equals(sourceIP) ) {
+            result.add(targetList.get(0));
+        } else if (targetList.size() > 1) {
+            for (int i=1; i<targetList.size(); i++) {
+                result.add(targetList.get(i));
+            }
+        }
+        return result;
+    }
+
+}
