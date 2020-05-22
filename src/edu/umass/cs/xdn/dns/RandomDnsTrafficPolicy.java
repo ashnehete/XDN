d42b9cf43ebc4f3d8305ce0c3e76a69ef5e218bc
Zhaoyu Gao <gaozy@cs.umass.edu>
2020-05-22 17:17:19 -0400

    add 2 more cloudlab nodes

---
+++ src/edu/umass/cs/xdn/dns/RandomDnsTrafficPolicy.java
@@ -0,0 +1,19 @@
+package edu.umass.cs.xdn.dns;
+
+import edu.umass.cs.reconfiguration.dns.DnsTrafficPolicy;
+
+import java.net.InetAddress;
+import java.util.*;
+
+public class RandomDnsTrafficPolicy implements DnsTrafficPolicy {
+    static Random rand = new Random();
+
+    @Override
+    public Set<InetAddress> getAddresses(Set<InetAddress> addresses, InetAddress source) {
+        Set<InetAddress> result = new HashSet<>();
+        List<InetAddress> targetList = new ArrayList<>(addresses);
+        result.add(targetList.get(rand.nextInt(targetList.size())));
+        
+        return result;
+    }
+}
