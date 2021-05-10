0ccfa4bf22b66b8944e9a9da3d05139361219c96
Zhaoyu Gao <gaozy@cs.umass.edu>
2021-05-10 09:59:13 -0400

    update latency test client

--- src/edu/umass/cs/xdn/tools/ExecuteRequestClient.java
+++ src/edu/umass/cs/xdn/tools/ExecuteRequestClient.java
@@ -1,5 +1,6 @@
 package edu.umass.cs.xdn.tools;
 
+import edu.umass.cs.gigapaxos.PaxosConfig;
 import edu.umass.cs.gigapaxos.interfaces.Request;
 import edu.umass.cs.reconfiguration.http.HttpActiveReplicaPacketType;
 import edu.umass.cs.reconfiguration.http.HttpActiveReplicaRequest;
@@ -7,6 +8,7 @@ import edu.umass.cs.xdn.XDNConfig;
 import edu.umass.cs.xdn.deprecated.XDNAgentClient;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.Random;
 
 public class ExecuteRequestClient {
@@ -17,6 +19,7 @@ public class ExecuteRequestClient {
     String value;
     boolean coord;
     int numReq;
+    String target;
 
     int id;
 
@@ -30,7 +33,15 @@ public class ExecuteRequestClient {
         imageName = XDNConfig.prop.getProperty(XDNConfig.XC.IMAGE_NAME.toString());
         value = XDNConfig.prop.getProperty(XDNConfig.XC.VALUE.toString());
         coord = Boolean.parseBoolean(XDNConfig.prop.getProperty(XDNConfig.XC.COORD.toString()));
-        numReq = Integer.parseInt(XDNConfig.prop.getProperty(XDNConfig.XC.NUM_REQ.toString()));
+
+        numReq = 5;
+        if ( XDNConfig.prop.getProperty(XDNConfig.XC.NUM_REQ.toString()) != null)
+            numReq = Integer.parseInt(XDNConfig.prop.getProperty(XDNConfig.XC.NUM_REQ.toString()));
+
+        target = null;
+        if( XDNConfig.prop.getProperty(XDNConfig.XC.NUM_REQ.toString()) != null)
+            target = XDNConfig.prop.getProperty(XDNConfig.XC.NUM_REQ.toString());
+
 
         serviceName = XDNConfig.generateServiceName(imageName, name);
 
@@ -55,13 +66,26 @@ public class ExecuteRequestClient {
     }
 
     private void sendRequest() {
+        InetSocketAddress addr = null;
+        if ( target !=null ){
+            addr = PaxosConfig.getActives().get(target);
+        }
+
         for (int i=0; i<numReq; i++){
             Request result = null;
             long start = System.currentTimeMillis();
-            try {
-                result = client.sendRequest(getRequest(), timeout);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (target == null)
+                try {
+                    result = client.sendRequest(getRequest(), timeout);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            else{
+                try {
+                    result = client.sendRequest(getRequest(), addr, timeout);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             if (result == null){
                 System.out.println(i+"th request:"+"timed out");
